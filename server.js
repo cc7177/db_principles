1f47cd9033cfb311c77156f7571949c09f883b6c
cc7177 <cc7177@nyu.edu>
2024-04-29 19:20:02 -0400

    Refactoring

--- server.js
+++ server.js
@@ -33,44 +33,20 @@ db.connect((err) => {
 });
 
 //Routes
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '/index.html'));
+app.get('/getPage/:table', function(req, res) {
+    res.sendFile(path.join(__dirname, `/${req.params.table}.html`));
   });
 
-app.get('/customers', function(req, res) {
-    res.sendFile(path.join(__dirname, '/customers.html'));
+app.get('/getForm/add/:entity', function(req, res) {
+    res.sendFile(path.join(__dirname, `/add_${req.params.entity}.html`));
   });
 
-app.get('/rentals', function(req, res) {
-   res.sendFile(path.join(__dirname, '/rentals.html'));
-  });
-
-app.get('/add_vehicle_form', function(req, res) {
-    res.sendFile(path.join(__dirname, '/add_vehicle.html'));
-  });
-
-app.get('/add_customer_form', function(req, res) {
-    res.sendFile(path.join(__dirname, '/add_customer.html'));
-  });
-
-app.get('/add_rental_form', function(req, res) {
-   res.sendFile(path.join(__dirname, '/add_rental.html'));
-  });
-
-app.get('/update_vehicle_form', function(req, res) {
-    res.sendFile(path.join(__dirname, '/update_vehicle.html'));
-  });
-
-app.get('/update_customer_form', function(req, res) {
-    res.sendFile(path.join(__dirname, '/update_customer.html'));
-  });
-
-app.get('/update_rental_form', function(req, res) {
-    res.sendFile(path.join(__dirname, '/update_rental.html'));
+app.get('/getForm/update/:entity', function(req, res) {
+    res.sendFile(path.join(__dirname, `/update_${req.params.entity}.html`));
   });
 
 
-app.get('/api/get_table/:table', (req, res) => {
+app.get('/api/getTable/:table', (req, res) => {
     const tableName = req.params.table;
     db.query(`SELECT * FROM ${tableName}`, (err, results) => {
         if(err) {
@@ -82,7 +58,7 @@ app.get('/api/get_table/:table', (req, res) => {
     });
 });
 
-app.get('/api/get_record/:table', (req, res) => {
+app.get('/api/getRecord/:table', (req, res) => {
     const tableName = req.params.table;
     const columnName = req.query.column;
     const columnValue = req.query.value;
@@ -153,133 +129,12 @@ app.post('/api/createRecord/:table', (req, res) => {
     });
   });
 
-app.post('/api/addVehicle', (req, res) => {
-    const LicensePlate=req.body.licensePlate;
-    const Make=req.body.make;
-    const Model=req.body.model;
-    const Year=req.body.year;
-    const Color=req.body.color;
-    const VehicleType=req.body.vehicleType;
-    const Status=req.body.status;
-    const Mileage=req.body.mileage;
+app.delete('/api/removeRecord/:table', function(req, res) {
+    const tableName = req.params.table;
+    const columnName = req.query.column;
+    const columnValue = req.query.value;
 
-    db.query('INSERT INTO Vehicles (licenseplate, make, model, year, color, vehicletype, status, mileage) '
-           + 'VALUES ($1,$2,$3,$4,$5,$6,$7,$8)', [LicensePlate, Make, Model, Year, Color, VehicleType, Status, Mileage], (err, results) => {
-        if(err) {
-            console.error('Error executing query...', err);
-            res.status(500).json({error: 'Internal Server Error'})
-            return;
-        }
-    });
-    res.redirect('/');
-});
-
-app.post('/api/addCustomer', (req, res) => {
-    const FirstName=req.body.firstname;
-    const LastName=req.body.lastname;
-    const DriverLicense=req.body.driverlicensenumber;
-    const Email=req.body.email;
-    const Phone=req.body.phone;
-    const Address=req.body.address;
-
-    db.query('INSERT INTO customers (firstname, lastname, driverlicensenumber, email, phone, address) '
-           + 'VALUES ($1,$2,$3,$4,$5,$6)', [FirstName, LastName, DriverLicense, Email, Phone, Address], (err, results) => {
-        if(err) {
-            console.error('Error executing query...', err);
-            res.status(500).json({error: 'Internal Server Error'})
-            return;
-        }
-    });
-    res.redirect('/customers');
-});
-
-app.post('/api/addRental', (req, res) => {
-    const VehicleID=req.body.vehicleid;
-    const CustomerID=req.body.customerid;
-    const StartDate=req.body.startdate;
-    const EndDate=req.body.enddate;
-    const ActualEndDate=req.body.actualenddate;
-    const DailyRate=req.body.dailyrate;
-    const TotalCost=req.body.totalcost;
-
-    db.query('INSERT INTO rentals (vehicleid, customerid, startdate, enddate, actualenddate, dailyrate, totalcost) '
-           + 'VALUES ($1,$2,$3,$4,$5,$6,$7)', [VehicleID, CustomerID, StartDate, EndDate, ActualEndDate, DailyRate, TotalCost], (err, results) => {
-        if(err) {
-            console.error('Error executing query...', err);
-            res.status(500).json({error: 'Internal Server Error'})
-            return;
-        }
-    });
-    res.redirect('/rentals');
-});
-
-app.post('/api/updateVehicle', (req, res) => {
-    const VehicleID=req.body.vehicleID;
-    const LicensePlate=req.body.licensePlate;
-    const Make=req.body.make;
-    const Model=req.body.model;
-    const Year=req.body.year;
-    const Color=req.body.color;
-    const VehicleType=req.body.vehicleType;
-    const Status=req.body.status;
-    const Mileage=req.body.mileage;
-
-    db.query('UPDATE Vehicles SET LicensePlate=$1, Make=$2, Model=$3, Year=$4, Color=$5, VehicleType=$6, Status=$7, Mileage=$8 WHERE VehicleID=$9'
-             , [LicensePlate, Make, Model, Year, Color, VehicleType, Status, Mileage, VehicleID], (err, results) => {
-        if(err) {
-            console.error('Error executing query...', err);
-            res.status(500).json({error: 'Internal Server Error'})
-            return;
-        }
-    });
-    res.redirect('/');
-});
-
-app.post('/api/updateCustomer', (req, res) => {
-    const CustomerID=req.body.customerID;
-    const FirstName=req.body.firstname;
-    const LastName=req.body.lastname;
-    const DriverLicense=req.body.driverlicensenumber;
-    const Email=req.body.email;
-    const Phone=req.body.phone;
-    const Address=req.body.address;
-
-    db.query('UPDATE customers SET fistname=$1, lastname=$2, driverlicensenumber=$3, email=$4, phone=$5, address=$6 WHERE customerid=$7'
-             , [FirstName, LastName, DriverLicense, Email, Phone, Address, CustomerID], (err, results) => {
-        if(err) {
-            console.error('Error executing query...', err);
-            res.status(500).json({error: 'Internal Server Error'})
-            return;
-        }
-    });
-    res.redirect('/customers');
-});
-
-app.post('/api/updateRental', (req, res) => {
-    const RentalID=req.body.rentalID;
-    const VehicleID=req.body.vehicleid;
-    const CustomerID=req.body.customerid;
-    const StartDate=req.body.startdate;
-    const EndDate=req.body.enddate;
-    const ActualEndDate=req.body.actualenddate;
-    const DailyRate=req.body.dailyrate;
-    const TotalCost=req.body.totalcost;
-
-    db.query('UPDATE rentals SET vehicleid=$1, customerid=$2, startdate=$3, enddate=$4, actualenddate=$5, dailyrate=$6, totalcost=$7 WHERE rentalid=$8'
-             , [VehicleID, CustomerID, StartDate, EndDate, ActualEndDate, DailyRate, TotalCost, RentalID], (err, results) => {
-        if(err) {
-            console.error('Error executing query...', err);
-            res.status(500).json({error: 'Internal Server Error'})
-            return;
-        }
-    });
-    res.redirect('/rentals');
-});
-
-app.delete('/api/removeVehicle', (req, res) => {
-    const VehicleID=req.query.id;
-
-    db.query('DELETE FROM Vehicles WHERE VehicleID=$1', [VehicleID], (err, results) => {
+    db.query(`DELETE FROM ${tableName} WHERE ${columnName}=$1`, [columnValue], (err, results) => {
         if(err) {
             console.error('Error executing query...', err);
             res.status(500).json({error: 'Internal Server Error'})
@@ -287,34 +142,8 @@ app.delete('/api/removeVehicle', (req, res) => {
         }
     });
     return;
-});
 
-app.delete('/api/removeCustomer', (req, res) => {
-    const CustomerID=req.query.id;
-
-    db.query('DELETE FROM customers WHERE customerid=$1', [CustomerID], (err, results) => {
-        if(err) {
-            console.error('Error executing query...', err);
-            res.status(500).json({error: 'Internal Server Error'})
-            return;
-        }
-    });
-    return;
-});
-
-app.delete('/api/removeRental', (req, res) => {
-    const RentalID=req.query.id;
-
-    db.query('DELETE FROM rentals WHERE rentalid=$1', [RentalID], (err, results) => {
-        if(err) {
-            console.error('Error executing query...', err);
-            res.status(500).json({error: 'Internal Server Error'})
-            return;
-        }
-    });
-    return;
-});
-
+  });
 
 // Start server
 app.listen(PORT, () => {
