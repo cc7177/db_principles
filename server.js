ef7ffa501b8772ad23ef3eba290b1f2d4e75fbb7
cc7177 <cc7177@nyu.edu>
2024-05-04 13:47:22 -0400

    testing

--- server.js
+++ server.js
@@ -32,7 +32,23 @@ db.connect((err) => {
     console.log(`Connected to ${DB_NAME}!`);
 });
 
+function convertDate(isoDate) {
+  const date = new Date(isoDate);
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate().padStart(2, '0'));
+  const year = date.getFullYear();
+
+  return `${month}-${day}-${year}`;
+}
+
 //Routes
+app.post('/convert-date', (req, res) => {
+  const isoDate = req.body.isoDate;
+  const formattedDate = convertDate(isoDate);
+
+  res.send(formattedDate);
+});
+
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '/index.html'));
   });
@@ -174,6 +190,7 @@ app.delete('/api/removeRecord/:table', function(req, res) {
     return;
 
   });
+
 
 // Start server
 app.listen(PORT, () => {
